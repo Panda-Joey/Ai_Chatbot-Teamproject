894440b56940e4e6513fe70871c48648141a09cc
IsabellaGale1 <131244215+IsabellaGale1@users.noreply.github.com>
2026-01-21 12:47:00 +0100

    formulier

---
+++ TP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vraag indienen</title>
+    <link rel="stylesheet" href="TP.css">
+</head>
+<body>
+
+<h2>Stel je vraag</h2>
+
+<form action="#" method="post">
+
+    <label>
+        Vraag:<br>
+        <textarea name="vraag" rows="4" required></textarea>
+    </label><br><br>
+
+    <button type="submit">Versturen</button>
+</form>
+
+</body>
+</html>
